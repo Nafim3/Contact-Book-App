b8aea50d22d7c820db8f6dec0247a24cfa5e305a
Nafim <M.R.NAFIM-2022@hull.ac.uk>
2025-06-20 12:10:29 +0100

    Implemented delete, edit operations

--- Controllers/ContactEntryController.cs
+++ Controllers/ContactEntryController.cs
@@ -31,5 +31,58 @@ namespace Contact_Book_App.Controllers
             Database.SaveChanges(); // Save changes to the database
             return RedirectToAction("Index");
         }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id==0)
+            {
+                return NotFound();
+            }
+
+            ContactEntry? contactEntry = Database.ContactEntries.Find(id);
+
+            if (contactEntry == null)
+            {
+                return NotFound();
+            }
+
+            return View(contactEntry);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(ContactEntry CEO)
+        {
+            Database.ContactEntries.Update(CEO); // updates the new contact entry to the database context
+            Database.SaveChanges(); // Save changes to the database
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            ContactEntry? contactEntry = Database.ContactEntries.Find(id);
+
+            if (contactEntry == null)
+            {
+                return NotFound();
+            }
+
+            return View(contactEntry);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(ContactEntry CEO)
+        {
+            Database.ContactEntries.Remove(CEO); // updates the new contact entry to the database context
+            Database.SaveChanges(); // Save changes to the database
+            return RedirectToAction("Index");
+        }
+
     }
 }
