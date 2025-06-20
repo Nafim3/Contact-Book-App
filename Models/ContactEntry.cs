6d6c5540972743047f99c00098e4cd4310b33f85
Nafim <M.R.NAFIM-2022@hull.ac.uk>
2025-06-20 05:34:14 +0100

    Implemented client-side validation

--- Models/ContactEntry.cs
+++ Models/ContactEntry.cs
@@ -5,18 +5,21 @@ namespace Contact_Book_App.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="The First Name field is required")]
         public string? FirstName { get; set; } = string.Empty;
 
-        [Required]
+        [Required(ErrorMessage ="The Last Name field is required")]
         public string? LastName { get; set; }
-        [Required]
+
+        [Required(ErrorMessage ="The Phone Number field is required")]
+        [RegularExpression(@"^\d{11}$", ErrorMessage = "Phone number must be exactly 11 digits")]
         public string? PhoneNumber { get; set;}
 
-        [EmailAddress]
+        [Required(ErrorMessage = "The Email field is required")]
+        [EmailAddress(ErrorMessage ="Enter a valid email address")]
         public string? Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Select a date from the calendar shown on the right side of the box above")]
         public DateTime Generated { get; set; } = DateTime.Now;
 
         
